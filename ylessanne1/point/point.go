e527e5ecbd4caf480c7f4c303cae945fbec972af
keijoraamat <raamtkeijo@gmail.com>
2023-09-12 08:19:09 +0300

    implemented commands

--- ylessanne1/point/point.go
+++ ylessanne1/point/point.go
@@ -14,14 +14,56 @@ func NewPoint(x, y float64) *Point {
 	return &Point{x: x, y: y}
 }
 
+func (p *Point) X() float64 {
+	return p.x
+}
+
+func (p *Point) Y() float64 {
+	return p.y
+}
+
 func (p *Point) String() string {
 	return fmt.Sprintf("x: %f\ny: %f\n", p.x, p.y)
 }
 
 func (p *Point) Roh() float64 {
+	//Distance to origin (0.0, 0.0)
 	return math.Sqrt(math.Pow(float64(p.x), 2) + math.Pow(p.y, 2))
 }
 
 func (p *Point) Theta() float64 {
+	//Angle to horisontal axis
 	return math.Atan2(p.y, p.x)
+}
+
+func (p *Point) Distance(other *Point) float64 {
+	//Distance to other
+	v := p.VectorTo(*other)
+	return v.Roh()
+}
+
+func (p *Point) VectorTo(other Point) Point {
+	//Point representing the vector from selt to other point
+	return *NewPoint(other.x-p.x, other.y-p.y)
+}
+
+func (p *Point) Translate(dx float64, dy float64) {
+	p.x += dx
+	p.y += dy
+}
+
+func (p *Point) Scale(factor int32) {
+	p.x *= float64(factor)
+	p.y *= float64(factor)
+}
+
+func (p *Point) CentreRotate(angle float64) {
+	p.x = p.Roh() * math.Cos(p.Theta()+angle)
+	p.y = p.Roh() * math.Sin(p.Theta()+angle)
+}
+
+func (p *Point) Rotate(pnt Point, angle float64) {
+	p.Translate(-pnt.X(), -pnt.Y())
+	p.CentreRotate(angle)
+	p.Translate(pnt.X(), pnt.Y())
 }
--- ylessanne1/point/point_test.go
+++ ylessanne1/point/point_test.go
@@ -7,6 +7,24 @@ import (
 	"github.com/keijoraamat/IDU1550/ylessanne1/point"
 )
 
+func TestPoint_X_Should_Return_Abscissa_Of_Point(t *testing.T){
+	p := point.NewPoint(7.778, 5.0)
+	want := 7.778
+
+	if got := p.X(); got != want {
+		t.Errorf("X() = %f, want %f", got, want)
+	}
+}
+
+func TestPoint_Y_Should_Return_Ordinate_Of_Point(t *testing.T){
+	p := point.NewPoint(1.1, -89.076)
+	want := -89.076
+
+	if got := p.Y(); got != want {
+		t.Errorf("X() = %f, want %f", got, want)
+	}
+}
+
 func TestPoint_Distance_To_Origin_Should_Be_As_Expected(t *testing.T) {
 	p := point.NewPoint(0.0, 5.0)
 	want := 5.0
@@ -23,7 +41,7 @@ func TestPoint_Distance_To_Origin_Should_Be_As_Expected(t *testing.T) {
 	}
 }
 
-func TestPoing_Angle_To_Horizontal_Axis_Should_Be_As_Expected(t *testing.T) {
+func TestPoint_Angle_To_Horizontal_Axis_Should_Be_As_Expected(t *testing.T) {
 	p := point.NewPoint(0.0, 0.0)
 	want := 0.0
 
@@ -36,5 +54,37 @@ func TestPoing_Angle_To_Horizontal_Axis_Should_Be_As_Expected(t *testing.T) {
 
 	if got2 := p2.Theta(); got2 != want2 {
 		t.Errorf("Theta() = %f, want %f", got2, want2)
+	}
+}
+
+func TestPoint_VectorTo_Should_Return_Point_Representing_Vector_To_Other_Point(t *testing.T){
+	p := point.NewPoint(1.0, 0.0)
+	other := point.NewPoint(2.0, 3.0)
+	want := point.NewPoint(1.0, 3.0)
+
+	if got := p.VectorTo(*other); got.X() != want.X() && got.Y() != want.Y(){
+		t.Errorf("VectorTo() = %f, want %f", got, want)
+	}
+}
+
+func TestPoint_Distance_Should_Return_Distance_Between_Points_When_Given_Two_Points(t *testing.T) {
+	p := point.NewPoint(0.0, 0.0)
+	other := point.NewPoint(0.0, -1.0)
+	want := 1.0
+
+	if got := p.Distance(other); got != want {
+		t.Errorf("Distance() = %f, want %F", got, want)
+	}
+}
+
+func TestPoint_Translate_Should_Move_Point_By_Given_Step(t *testing.T){
+	p := point.NewPoint(1.0, -1.0)
+	wantX := 0.5
+	wantY := 0.0
+
+    p.Translate(-0.5, 1.0)
+
+	if  (p.X() != wantX || p.Y() != wantY){
+		t.Errorf("Translate() = {%f, %f}, want {%f, %f}", p.X(), p.Y(), wantX, wantY)
 	}
 }
